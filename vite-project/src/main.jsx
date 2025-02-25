e9f3d3879ea564a7ef2819501af3b8ad0f632667
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2025-02-25 20:38:51 +0600

    25th feb

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,18 @@
+// import ReactDOM from "react-dom"
+// import App from "./App"
+
+// ReactDOM.render(<App/>, document.getElementById("root"))
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+    <App />  
+// this is optional
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+);
