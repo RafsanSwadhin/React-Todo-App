b12ade53a52da069e9106ed2448e494a6d879d68
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2025-02-26 17:25:31 +0600

    26th feb

---
+++ vite-project/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const dummyTodos = {
+    id : 1,
+    title : "Todo title",
+    desc : " Todo description is here "
+}
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
