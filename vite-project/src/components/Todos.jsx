f3f87023e30c30f2bbf18e0d5c3385b572b2a931
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2025-04-24 23:10:29 +0600

    practice todos

--- vite-project/src/components/Todos.jsx
+++ vite-project/src/components/Todos.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import Todo from './Todo'; // Ensure you have this component
-import style from "./todos.module.css"
-const Todos = (props) => {
-  console.log(props.todos);
+import React from "react";
+
+import Todo from './Todo'
+const Todos = (props) =>{
+  console.log(props.todos)
 
   return (
-    <section className={style.todos}>
+    <section>
       {
-        props.todos.map((todo) => (  // ✅ Corrected arrow function syntax
-          <Todo todo={todo.todo} key={todo.id} />
-        ))
+        props.todos.map((todo ) =><Todo todo={todo} key = {todo.id}/>)
       }
     </section>
-  );
-};
+  )
+}
 
-export default Todos;
+// import React from 'react';
+// import Todo from './Todo'; // Ensure you have this component
+// import style from "./todos.module.css"
+// const Todos = (props) => {
+//   console.log(props.todos);
+
+//   return (
+//     <section className={style.todos}>
+//       {
+//         props.todos.map((todo) => (  // ✅ Corrected arrow function syntax
+//           <Todo todo={todo.todo} key={todo.id} />
+//         ))
+//       }
+//     </section>
+//   );
+// };
+
+// export default Todos;
